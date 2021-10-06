71d25661d4e00e748e4640aa90bf41a5622f44b5
Arkadiy <arkadiy.garanin@socialsys.ru>
2021-10-06 23:19:07 +0300

    Добавился импорт в лямбда тесте

--- src/test/java/com/gmail/arkgaranin/tests/LambdaStepsTest.java
+++ src/test/java/com/gmail/arkgaranin/tests/LambdaStepsTest.java
@@ -1,11 +1,12 @@
-package com.gmail.arkgaranin;
+package com.gmail.arkgaranin.tests;
 
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 public class LambdaStepsTest extends BaseTest {
